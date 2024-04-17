17e6d0eb5ff9a94217733eeddcd400c0e0d04f90
Nicolas Nievas <nicoonievas7@gmail.com>
2024-04-17 17:17:18 -0300

    personajes

--- clase-03/src/app/characters/page.tsx
+++ clase-03/src/app/characters/page.tsx
@@ -1,8 +1,56 @@
-//pagina rapida para personajes 
-import React from 'react';
-import { useEffect, useState, useRef } from 'react';
-import { getBookData, getChapterDataForCharacters } from '@/services/RestServices';
+"use client";
+import React, { useState, useEffect } from "react";
+import { getBookData, getChapterDataForCharacters} from "@/services/RestServices";
 
-export default function charapter() {
-    <h2>personajes</h2>
+export default function CharactersPage() {
+  const [characters, setCharacters] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const bookData = await getBookData();
+        const chapterData = await getChapterDataForCharacters(bookData);
+        const allCharacters = chapterData.reduce((acc, book) => {
+          return [
+            ...acc,
+            ...book.characters.map((character: any) => ({
+              ...character,
+              book: book.name,
+            })),
+          ];
+        }, []);
+
+        setCharacters(allCharacters);
+      } catch (error) {
+        console.error("Error fetching characters data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container mx-auto py-4 px-6 mt-16">
+      <h2 className="text-center mb-8 text-3xl font-bold">Personajes</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {characters.map((character, index) => (
+          <div key={index} className="mb-8">
+            <div className="border border-gray-300 p-4">
+              <strong>{character.name}</strong>
+              <div className="mt-2">
+                {character.gender && <div>Género: {character.gender}</div>}
+                {character.titles && character.titles.length > 0 && (
+                  <div>Título: {character.titles[0]}</div>
+                )}
+                {character.aliases && character.aliases.length > 0 && (
+                  <div>Alias: {character.aliases[0]}</div>
+                )}
+                {character.book && <div>Libro: {character.book}</div>}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- clase-03/src/app/page.tsx
+++ clase-03/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { useEffect, useState, useRef } from 'react';
-import { getBookData, getChapterDataForCharacters } from '@/services/RestServices';
+import { useEffect, useState, useRef } from "react";
+import { getBookData, getChapterDataForCharacters} from "@/services/RestServices";
+import Container from "@/app/_components/container";
 
 export default function Home() {
   const [books, setBooks] = useState<any[]>([]);
@@ -12,46 +13,60 @@ export default function Home() {
       const chapterData = await getChapterDataForCharacters(bookData);
       setBooks(chapterData);
 
-      window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: "smooth" });
     };
     fetchData();
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="mt-8 mb-4 text-3xl font-bold">Libros</h1>
-      <ul>
-        {books.map((book, index) => (
-          <li key={index} className="mb-8">
-            <div className="border border-gray-300 p-4">
-              <div className="flex justify-between mb-2">
-                <div>
-                  <strong>{book.name}</strong>
+    <main>
+      <Container>
+        <div className="flex flex-col items-center justify-center mt-16">
+          <h1 className="mt-8 mb-4 text-3xl font-bold">Libros</h1>
+          <ul>
+            {books.map((book, index) => (
+              <li key={index} className="mb-8">
+                <div className="border border-gray-300 p-4">
+                  <div className="flex justify-between mb-2">
+                    <div>
+                      <strong>{book.name}</strong>
+                    </div>
+                    <div>
+                      {book.authors} - {book.country}
+                    </div>
+                  </div>
+                  <details>
+                    <summary>Personajes</summary>
+                    <ul>
+                      {book.characters.map(
+                        (character: any, characterIndex: number) => (
+                          <li key={characterIndex}>
+                            <div className="border-t border-gray-300 pt-2 mt-2">
+                              <strong>{character.name}</strong>
+                              <div>
+                                Género: {character.gender}, Título:{" "}
+                                {character.titles && character.titles.length > 0
+                                  ? character.titles[0]
+                                  : "Sin título"}
+                                , Alias:{" "}
+                                {character.aliases &&
+                                character.aliases.length > 0
+                                  ? character.aliases[0]
+                                  : "Sin alias"}
+                              </div>
+                            </div>
+                          </li>
+                        )
+                      )}
+                    </ul>
+                  </details>
                 </div>
-                <div>
-                  {book.authors} - {book.country}
-                </div>
-              </div>
-              <details>
-                <summary>Personajes</summary>
-                <ul>
-                  {book.characters.map((character: any, characterIndex: number) => (
-                    <li key={characterIndex}>
-                      <div className="border-t border-gray-300 pt-2 mt-2">
-                        <strong>{character.name}</strong>
-                        <div>
-                          Género: {character.gender}, Título: {character.titles && character.titles.length > 0 ? character.titles[0] : 'Sin título'}, Alias: {character.aliases && character.aliases.length > 0 ? character.aliases[0] : 'Sin alias'}
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </details>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <div ref={footerRef}></div> 
-    </div>
+              </li>
+            ))}
+          </ul>
+          <div ref={footerRef}></div>
+        </div>
+      </Container>
+    </main>
   );
 }
