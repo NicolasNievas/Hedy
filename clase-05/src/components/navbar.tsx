0bd37b6b5509ef37ce5c1fcbbf67e0e2e64a6f64
Nicolas Nievas <nicoonievas7@gmail.com>
2024-05-16 20:03:14 -0300

    loading & navbar

---
+++ clase-05/src/components/navbar.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between">
+          <div>
+            <Link href="/">
+              <div className="text-white text-xl font-semibold cursor-pointer">Books</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
