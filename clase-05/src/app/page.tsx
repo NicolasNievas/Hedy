56f6896b3b1892d7ba84b1c5acc33c55e18e9729
Nicolas Nievas <nicoonievas7@gmail.com>
2024-05-18 23:35:01 -0300

    navbar

--- clase-05/src/app/page.tsx
+++ clase-05/src/app/page.tsx
@@ -4,13 +4,7 @@ import BookCard from '@/app/book/page';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form action={logout}>
-        <button type="submit" className="mb-4 px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-          Logout
-        </button>
-      </form>
-      <div className="bg-gray-100 py-12 sm:py-16">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-100">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="max-w-3xl mx-auto text-center">
             <h2 className="text-3xl font-bold text-gray-800 sm:text-4xl">Opinions about books</h2>
@@ -22,7 +16,6 @@ export default function Home() {
             ))}
           </div>
         </div>
-      </div>
     </main>
   );
 }
--- clase-05/src/components/navbar.tsx
+++ clase-05/src/components/navbar.tsx
@@ -1,6 +1,25 @@
+"use client"
 import Link from 'next/link';
+import { useDataContext } from "@/context/DataContext";
+import { logout } from "@/app/logout/actions";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react'; 
 
 const Navbar = () => {
+  const { user } = useDataContext();
+  const router = useRouter();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(!!user);
+
+  useEffect(() => {
+    setIsLoggedIn(!!user); 
+  }, [user]);
+
+  const handleLogout = async () => {
+    await logout();
+    router.push('/');
+  };
+
   return (
     <nav className="bg-gray-800 py-4">
       <div className="container mx-auto px-4">
@@ -9,6 +28,20 @@ const Navbar = () => {
             <Link href="/">
               <div className="text-white text-xl font-semibold cursor-pointer">Books</div>
             </Link>
+          </div>
+          <div className="flex items-center">
+            {isLoggedIn ? (
+              <>
+                <p className="text-white mr-4">{`Hello ${user?.email}`}</p> {/* Mostrar el correo electrónico del usuario si está logueado */}
+                <button onClick={handleLogout} className="text-white text-sm font-medium px-4 py-2 border border-transparent rounded-md shadow-sm bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                  Logout
+                </button>
+              </>
+            ) : (
+              <Link href="/login">
+                <div className="text-white text-sm font-medium px-4 py-2 border border-transparent rounded-md shadow-sm bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</div>
+              </Link>
+            )}
           </div>
         </div>
       </div>
