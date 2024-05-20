b8cf80ba0d2b74fc77566eec43c0d7e71a8fbc3e
Nicolas Nievas <nicoonievas7@gmail.com>
2024-05-19 23:27:58 -0300

    private page

--- clase-05/src/app/page.tsx
+++ clase-05/src/app/page.tsx
@@ -3,11 +3,12 @@ import BookCard from '@/app/book/page';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import Loading from '@/components/loading';
+import Link from 'next/link';
 
 export default async function Home() {
   const supabase = createClient();
-
   const { data, error } = await supabase.auth.getUser();
+
   if (error || !data?.user) {
     return (
       <>
@@ -21,9 +22,11 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-100">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative">
         <div className="absolute top-0 right-0 mt-4 mr-4 text-gray-600 bg-gray-200 bg-opacity-75 rounded-lg px-4 py-2">
-          <p>
-            signed in as <span className="font-semibold">{data.user.email}</span>
-          </p>
+          <Link href="/private">
+            <p className="cursor-pointer">
+              signed in as <span className="font-semibold">{data.user.email}</span>
+            </p>
+          </Link>
         </div>
         <div className="max-w-3xl mx-auto text-center">
           <h2 className="text-3xl font-bold text-gray-800 sm:text-4xl">Opinions about books</h2>
