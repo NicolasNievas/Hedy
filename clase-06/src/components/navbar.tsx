431231eeba92cfe1494ca1024708a9027ed52e3a
Nicolas Nievas <nicoonievas7@gmail.com>
2024-05-21 21:10:05 -0300

    navbar & about us

--- clase-06/src/components/navbar.tsx
+++ clase-06/src/components/navbar.tsx
@@ -1,28 +1,39 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
 import Flag_ES from "../public/es_flag.png";
 import Flag_PT from "../public/pt_flag.png";
 import Flag_UK from "../public/uk_flag.png";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useTranslations } from "next-intl";
 
 export default function Navbar() {
-  return (
-    <nav className="flex items-center justify-between w-full p-4 bg-gray-800 text-white">
-        <div className="flex items-center space-x-4">
-        <Link href="/"><p>Home</p></Link>
-        <Link href="/about"><p>About</p></Link>
-        <Link href="/contact"><p>Contact</p></Link>
-      </div>
-      <div className="flex items-center space-x-4">
-        <Link href="/es">
-            <Image src={Flag_ES} alt="ES Flag" width={30} height={30} />
-        </Link>
-        <Link href="/pt">
-            <Image src={Flag_PT} alt="PT Flag" width={30} height={30} />
-        </Link>
-        <Link href="/uk">
-            <Image src={Flag_UK} alt="UK Flag" width={30} height={30} />
-        </Link>
-      </div>
-    </nav>
-  );
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const locale = pathname.split('/')[1];
+    const t = useTranslations('Navbar');
+
+    return (
+        <nav className="flex items-center justify-between w-full p-4 bg-gray-800 text-white">
+            <div className="flex items-center space-x-4">
+                <Link href={`/${locale}`}>
+                    <p>{t('home')}</p>
+                </Link>
+                <Link href={`/${locale}/about`}>
+                    <p>{t('about')}</p>
+                </Link>
+            </div>
+            <div className="flex items-center space-x-4">
+                <Link href="/es">
+                    <Image src={Flag_ES} alt="ES Flag" width={30} height={30} />
+                </Link>
+                <Link href="/pt">
+                    <Image src={Flag_PT} alt="PT Flag" width={30} height={30} />
+                </Link>
+                <Link href="/uk">
+                    <Image src={Flag_UK} alt="UK Flag" width={30} height={30} />
+                </Link>
+            </div>
+        </nav>
+    );
 }
