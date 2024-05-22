e07b93d428be6d967a7ab8df7786bfed02b5dc39
Nicolas Nievas <nicoonievas7@gmail.com>
2024-05-21 21:49:59 -0300

    mantenimiento app

--- clase-06/src/components/navbar.tsx
+++ clase-06/src/components/navbar.tsx
@@ -4,33 +4,34 @@ import Image from "next/image";
 import Flag_ES from "../public/es_flag.png";
 import Flag_PT from "../public/pt_flag.png";
 import Flag_UK from "../public/uk_flag.png";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useTranslations } from "next-intl";
 
 export default function Navbar() {
     const pathname = usePathname();
-    const searchParams = useSearchParams();
-    const locale = pathname.split('/')[1];
+    const segments = pathname.split('/');
+    const currentLocale = segments[1]; 
+    const currentPath = segments.slice(2).join('/'); 
     const t = useTranslations('Navbar');
 
     return (
         <nav className="flex items-center justify-between w-full p-4 bg-gray-800 text-white">
             <div className="flex items-center space-x-4">
-                <Link href={`/${locale}`}>
+                <Link href={`/${currentLocale}`}>
                     <p>{t('home')}</p>
                 </Link>
-                <Link href={`/${locale}/about`}>
+                <Link href={`/${currentLocale}/about`}>
                     <p>{t('about')}</p>
                 </Link>
             </div>
             <div className="flex items-center space-x-4">
-                <Link href="/es">
+                <Link href={`/es/${currentPath}`}>
                     <Image src={Flag_ES} alt="ES Flag" width={30} height={30} />
                 </Link>
-                <Link href="/pt">
+                <Link href={`/pt/${currentPath}`}>
                     <Image src={Flag_PT} alt="PT Flag" width={30} height={30} />
                 </Link>
-                <Link href="/uk">
+                <Link href={`/uk/${currentPath}`}>
                     <Image src={Flag_UK} alt="UK Flag" width={30} height={30} />
                 </Link>
             </div>
