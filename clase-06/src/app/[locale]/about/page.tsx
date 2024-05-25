08fbb784f33c0eb850830331f3b371f1187a32f1
Nicolas Nievas <nicoonievas7@gmail.com>
2024-05-25 17:56:32 -0300

    styless

--- clase-06/src/app/[locale]/about/page.tsx
+++ clase-06/src/app/[locale]/about/page.tsx
@@ -1,12 +1,11 @@
-// app/[locale]/about/page.tsx
 import { useTranslations } from 'next-intl';
 
 export default function About() {
     const t = useTranslations('About');
     return (
-        <div>
-            <h1>{t('title')}</h1>
-            <p>{t('description')}</p>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+            <h1 className="text-4xl font-bold text-center mb-4">{t('title')}</h1>
+            <p className="text-lg text-center max-w-prose">{t('description')}</p>
         </div>
     );
 }
--- clase-06/src/app/[locale]/page.tsx
+++ clase-06/src/app/[locale]/page.tsx
@@ -1,6 +1,11 @@
-import {useTranslations} from 'next-intl';
- 
+import { useTranslations } from 'next-intl';
+
 export default function Index() {
-  const t = useTranslations('Home');
-  return <h1>{t('title')}</h1>;
+    const t = useTranslations('Home');
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+            <h1 className="text-4xl font-bold text-center mb-4">{t('title')}</h1>
+        </div>
+    );
 }
